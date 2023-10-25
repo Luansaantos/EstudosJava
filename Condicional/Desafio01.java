7bb551dd017622280aa0387476713f1c067abefd
Assis <deassiscaetano29@gmail.com>
2023-10-24 23:07:17 -0300

    Desafio01-Atualizado

---
+++ Condicional/Desafio01.java
@@ -0,0 +1,14 @@
+public class Desafio01{
+
+   /* Imprimir todos os números de forma crescente de 1 a 10 
+    usando for ao finaliza adicione um print do exercício aqui, 
+    suba o desafio para guit usando padrao de branch e pedir reviwer
+ */
+
+ public static void main(String[] args) {
+    
+    for(int i = 0; i < 10; i++){
+        System.out.println("Valor de i: " + i);
+    }
+ }
+}
